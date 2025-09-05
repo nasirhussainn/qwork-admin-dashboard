4fd584a2511b2d6f48ec1d75910143feb90c866d
Nasir <nasirhussaintormik@gmail.com>
2025-09-05 21:16:15 +0500

    Update Vite configuration to use production API endpoint

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://portalapp.digidine.us',
-        target: 'http://localhost:3000',
+        target: 'https://portalapp.digidine.us',
+        // target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false,
       }
